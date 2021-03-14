574546b82d573f2cdc19817be195ff31c2a3949e
Connie Yao <yaoconnie@outlook.com>
2021-03-13 22:35:16 -0500

    check cart in url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
+/*global chrome*/
 import './App.css';
 
+async function isCart(){
+  let result = '';
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    result = tabs[0];
+    // use `url` here inside the callback because it's asynchronous!
+  });
+
+  console.log(result?.url);
+
+  return result?.url.includes("cart");
+}
+
 function App() {
+  console.log(0, isCart());
   return (
     <div className="App">
       <div className="container">
@@ -9,6 +22,7 @@ function App() {
         Hi, Kim!
         </header>
       </div>
+      
     </div>
   );
 }
